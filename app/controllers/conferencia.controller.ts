013bd42cad5a6dd51ad451ad8855775aa05e2b3c
Cassia Moraes <ccassiamoraess@gmail.com>
2023-04-26 14:27:28 -0300

    Update conferencia.controller.ts

--- app/controllers/conferencia.controller.ts
+++ app/controllers/conferencia.controller.ts
@@ -2,10 +2,10 @@
 import { Conferencia } from "../models/conferencia.js"
 
  export class conferenciaController{
-    private inputNome
-    private inputNascimento
-    private inputValor
-    private imprime
+    private inputNome: HTMLInputElement
+    private inputNascimento: HTMLInputElement
+    private inputValor: HTMLInputElement
+    private imprime: HTMLDivElement
 
     constructor(){
         this.inputNome = document.querySelector('#nome')
@@ -15,15 +15,26 @@ import { Conferencia } from "../models/conferencia.js"
        
     }
 
-    verifica(){
-        const conferencia = new Conferencia(
-            this.inputNome.value,
-            this.inputNascimento.value,
-            this.inputValor.value,
-
-        )
-
+    verifica(): void {
+        const conferencia = this.criaVerificacao()
         console.log(conferencia)
         this.imprime.innerHTML = conferencia.dados
+        this.limpaFormulario()
+        
+    }
+  
+  criaVerificacao(): Conferencia{
+        const nome = String(this.inputNome.value)
+        const exp = /-/g
+        const nascimento = new Date(this.inputNascimento.value.replace(exp, ','))
+        const valor = parseFloat(this.inputValor.value)
+        return new Conferencia(nome, nascimento, valor)
+    }
+  
+  limpaFormulario(): void {
+        this.inputNome.value = ''
+        this.inputNascimento.value = ''
+        this.inputValor.value = ''
+        this.inputNome.focus()
     }
  }
